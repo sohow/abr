8eb922236218217bab70c9b8d7361734e27f63f7
sohow <805124313@qq.com>
2017-12-09 19:52:53 +0800

    Update e.py

--- etao/e.py
+++ etao/e.py
@@ -2,8 +2,9 @@
 from selenium import webdriver
 import time
 import json
+import sys
 
-def do(codes):
+def do(codes, tb_id):
 	try:
 		options = webdriver.ChromeOptions()
 		options.add_argument("--start-maximized")
@@ -15,7 +16,7 @@ def do(codes):
 				time.sleep(2)
 				driver.find_element_by_class_name("act-btn-single").click()
 				time.sleep(2)
-				driver.find_element_by_class_name("draw-user-name").send_keys(u"韩宗稳")
+				driver.find_element_by_class_name("draw-user-name").send_keys(tb_id)
 				time.sleep(2)
 				driver.find_element_by_class_name("draw-action").click()
 				time.sleep(2)
@@ -26,42 +27,64 @@ def do(codes):
 		driver.quit()
 		print(repr(e))
 
-def to_do(dict):
+def to_do(filename, tb_id):
 	codes = {}
-	for k, v in dict.items():
+	count = 0
+	urls_dict = get(filename)
+	#tb_id = tb_id.decode()
+	for k, v in urls_dict.items():
 		if (v == 0):
-			print(v)
 			continue
-		dict[k] = 0
+		urls_dict[k] = 0
 		codes[k] = v;
 		if (len(codes) >= 2):
 			try:
-				do(codes)
+				do(codes, tb_id)
 			except Exception as e:
 				print(repr(e))
 			codes = {}
+		count += 1
+		print ("count: %d" % count)
+		save(filename, urls_dict)
 
-def printc(dict):
+def get(filename):
+	try:
+		file = open(filename, 'r')
+		urls = file.read()
+		file.close()
+		urls_dict = json.loads(urls)
+	except IOError:
+		print("urls.txt文件不存在，请首先更新链接")
+	return urls_dict
+	
+def save(filename,urls_dict):
+	urls = json.dumps(urls_dict)
+	file = open(filename, 'w+')
+	file.write(urls)
+	file.close()
+
+def printc(filename):
 	c = 0
 	b = 0
-	for k, v in dict.items():
+	urls_dict = get(filename)
+	for k, v in urls_dict.items():
 		if (v == 0):
 			c += 1
 		else:
 			b += 1
-	print("0: %d" % c)
-	print("1: %d" % b)
+		#urls_dict[k] = 1
+	print("已刷: %d" % c)
+	print("未刷: %d" % b)
 
 if __name__ == "__main__":
-	file = open('urls.txt', 'r')
-	urls = file.read()
-	file.close()
-	dict = json.loads(urls)
+	filename = "urls.txt"
+	tb_id = sys.argv[1] if len(sys.argv) > 1 else 0
+	if (tb_id == 0):
+		print("淘宝id错误")
+	else:
+		print("start: %s" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
+		printc(filename)
+		to_do(filename, tb_id)
+		print("end: %s" % time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time())))
 
-	printc(dict)
-	to_do(dict)
-
-	urls = json.dumps(dict)
-	file = open('urls.txt', 'w+')
-	file.write(urls)
-	file.close()
+	
